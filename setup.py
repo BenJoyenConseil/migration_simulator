e15a04bd8368b936e2265531a912b84887f0ea11
BenJoyenConseil <benjamin.joyen@decathlon.com>
2023-06-30 23:16:06 +0200

    packaging code

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+with open('requirements.txt', 'r') as f:
+    req = [p for p in f.readlines()]
+
+setup(
+    name='mcsim',
+    version='1.1.0',
+    install_requires=[],
+    packages=find_packages(exclude='data')
+)
