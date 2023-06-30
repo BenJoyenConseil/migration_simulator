13718a17d7d193877a381dd052fc5f97e7613b05
BenJoyenConseil <benjamin.joyen@decathlon.com>
2023-06-30 23:43:43 +0200

    fix migration tests

--- montecarlo/test_migration.py
+++ montecarlo/test_migration.py
@@ -6,7 +6,7 @@ def test_simulator():
     np.random.seed(123)
     runs = 100
     
-    durations, workloads, progress = simulator(
+    result = simulator(
         runs=runs, 
         stream_num=4, 
         stream_workload=1, 
@@ -14,14 +14,14 @@ def test_simulator():
         people=5
     )
 
-    assert np.histogram(workloads, bins=[0, 1, 2, 3])[0].tolist() == [206, 106, 45]
-    assert np.average(workloads) == 1.3612425940303106
-    assert np.median(workloads) == 0.9493946174728486
-    assert round(np.sum(workloads) / runs) == 5
+    assert np.histogram(result["r_workloads"], bins=[0, 1, 2, 3])[0].tolist() == [206, 106, 45]
+    assert np.average(result["r_workloads"]) == 1.3612425940303106
+    assert np.median(result["r_workloads"]) == 0.9493946174728486
+    assert round(np.sum(result["r_workloads"]) / runs) == 5
 
-    assert np.percentile(durations, 75) == 6.
+    assert np.percentile(result["r_durations"], 75) == 6.
 
-    assert progress[0] == {
+    assert result["r_progress_by_run"][0] == {
         "iterations": [0, 1, 2, 3, 4],
         "progress": [0, 3.6232803433237253, 3.3538809332847217, 2.086608770235758, 0]
     }
